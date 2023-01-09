7d6fb38de244862453ebc48eadef42e730c98ae5
Kakadu <Kakadu@pm.me>
2023-01-10 00:09:44 +0300

    Demo for generation of %%distrib from camlp5
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- demo5/main.ml
+++ demo5/main.ml
@@ -3,7 +3,7 @@ open Tester
 
 type 'a mystream = ('a, 'a mystream) Lib.Stream.t [@@deriving gt ~options:{show}]
 
-let prj_exn_mystream ra = 
+let prj_exn_mystream ra =
   let open Lib.Stream in
   let open Env.Monad in
   let open Env.Monad.Syntax in
@@ -18,3 +18,28 @@ let _ =
   let ys = cons !!30 (nil ()) in
   run_exn 1 q qh
     (REPR (fun q -> ocanren { Lib.append_o ((===)xs) ((===)ys) q } ))
+
+
+
+module _ = struct
+  ocanren type 'a lst = Nil | Cons of 'a * 'a lst
+
+
+  let rec appendo x y xy =
+    let open OCanren in
+    conde
+      [
+        x === inj Nil &&& (xy === y);
+        fresh (tmp h tmp2)
+          (x === cons h tmp)
+          (xy === cons h tmp2)
+          (appendo tmp y tmp2);
+      ]
+
+  let () =
+    OCanren.(
+      run q (fun xy -> appendo (inj Nil) (cons !!1 (cons !!2 (inj Nil))) xy))
+      (fun rr -> rr#reify (prj_exn OCanren.prj_exn))
+    |> OCanren.Stream.iter (fun xs ->
+           Format.printf "%s\n" (GT.show lst (GT.show GT.int) xs))
+end
