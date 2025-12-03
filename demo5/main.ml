ffe13d993dba9c6f83e536305667c397e070af92
Kakadu <Kakadu@pm.me>
2025-12-03 17:33:18 +0300

    Fix example for new noCanren
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- demo5/main.ml
+++ demo5/main.ml
@@ -1,10 +1,10 @@
 open OCanren
 open Tester
 
-type 'a mystream = ('a, 'a mystream) Lib.Stream.t [@@deriving gt ~options:{show}]
+type 'a mystream = ('a, 'a mystream) Lib.HO.Stream.t [@@deriving gt ~options:{show}]
 
 let prj_exn_mystream ra =
-  let open Lib.Stream in
+  let open Lib.HO.Stream in
   let open Env.Monad in
   let open Env.Monad.Syntax in
   Reifier.fix (fun self ->
@@ -13,11 +13,11 @@ let prj_exn_mystream ra =
 let run_exn eta = run_r (prj_exn_mystream OCanren.prj_exn) ([%show: GT.int mystream] ()) eta
 
 let _ =
-  let open Lib.Stream in
+  let open Lib.HO.Stream in
   let xs = cons !!1 (cons !!2 (nil())) in
   let ys = cons !!30 (nil ()) in
   run_exn 1 q qh
-    (REPR (fun q -> ocanren { Lib.append_o ((===)xs) ((===)ys) q } ))
+    (REPR (fun q -> ocanren { Lib.HO.append ((===)xs) ((===)ys) q } ))
 
 
 
@@ -41,5 +41,5 @@ module _ = struct
       run q (fun xy -> appendo (inj Nil) (cons !!1 (cons !!2 (inj Nil))) xy))
       (fun rr -> rr#reify (prj_exn OCanren.prj_exn))
     |> OCanren.Stream.iter (fun xs ->
-           Format.printf "%s\n" (GT.show lst (GT.show GT.int) xs))
+           Format.printf "%s\n" (GT.show ground (GT.show GT.int) xs))
 end
